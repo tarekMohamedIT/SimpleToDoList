ed3c8d0536109f845e35685cac4e3910080db928
Tarek Mohamed <tarek.moahmmed@synceg.com>
2020-06-21 01:53:14 +0200

    Added one item of the new checklist class in the MockNotesProvider

--- ToDoList.WindowsFormApp/MockNotesProvider.cs
+++ ToDoList.WindowsFormApp/MockNotesProvider.cs
@@ -5,22 +5,45 @@ using ToDoList.Core.Persistence.DataProviders;
 
 namespace ToDoList.WindowsFormApp
 {
-	public class MockNotesProvider : IDataProvider<Note>
+	public class MockNotesProvider : IDataProvider<BaseNote>
 	{
-		public IEnumerable<Note> Query => _table;
-		public IEnumerable<Note> ReadOnlyQuery => _table;
+		public IEnumerable<BaseNote> Query => _table;
+		public IEnumerable<BaseNote> ReadOnlyQuery => _table;
 
-		private List<Note> _table;
+		private List<BaseNote> _table;
 
 		public MockNotesProvider()
 		{
 
-			_table = new List<Note>(){
-					new Note() { Id = 1, CreationDate = DateTime.Now, Name = "Test1"},
-					new Note() { Id = 2, CreationDate = DateTime.Now, Name = "Test2"},
-					new Note() { Id = 3, CreationDate = DateTime.Now, Name = "Test3"},
-					new Note() { Id = 4, CreationDate = DateTime.Now, Name = "Test4"}
-			};
+			_table = new List<BaseNote>(){
+				new Note()
+				{
+					Id = 0,
+					Title = "A simple note",
+					Text = "Just a simple note",
+					CreationDate = DateTime.Now
+				},
+
+				new Note()
+				{
+				Id = 1,
+				Title = "A simple note",
+				Text = "Just a simple note",
+				CreationDate = DateTime.Now
+			},
+
+			new CheckList()
+			{
+				Id = 2,
+				Title = "A simple note",
+				Items = new List<ChecklistItem>()
+				{
+					new ChecklistItem(){Text = "Wake up", Checked = false},
+					new ChecklistItem(){Text = "Breakfast", Checked = false},
+					new ChecklistItem(){Text = "Workout", Checked = false},
+					new ChecklistItem(){Text = "Go to work", Checked = false}
+				}
+			}};
 		}
 
 		public void Save()
