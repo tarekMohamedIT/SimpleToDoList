c838e629c0dd421c8a107cc76ca8e247bcd98d7c
Tarek Mohamed <tarek.moahmmed@synceg.com>
2020-07-19 12:39:57 +0200

    Added a new context pattern to the windows form application

---
+++ ToDoList.WindowsFormApp/Contexts/FormsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList.WindowsFormApp.Contexts
+{
+	public class FormsContext
+	{
+		public static FormsContext Instance { get; } = new FormsContext();
+
+		public Form CurrentActiveForm { get; set; }
+
+		private FormsContext() { }
+	}
+}
