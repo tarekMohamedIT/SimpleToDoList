6c56c38f0c085cc2d3c108981ca800bb755d3e51
Tarek Mohamed <tarek.moahmmed@synceg.com>
2020-07-19 12:40:24 +0200

    Added a factory class for the sub menu items

---
+++ ToDoList.WindowsFormApp/Factories/SubMenuItemFactory.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ToDoList.WindowsFormApp.Models.SubMenuModels;
+
+namespace ToDoList.WindowsFormApp.Factories
+{
+	public class SubMenuItemFactory
+	{
+		public static SubMenuItemFactory Instance => new SubMenuItemFactory();
+
+		public SubMenuModel CreateMainMenuModel(string name, List<SubMenuModel> subItems)
+		{
+			return new SubMenuModel()
+			{
+				Name = name,
+				SubMenuItems = subItems
+			};
+		}
+
+		public SubMenuModel CreateSubMenuModel(string name, EventHandler handler)
+		{
+			return new SubMenuModel()
+			{
+				Name = name,
+				OnClick = handler,
+				SubMenuItems = null
+			};
+		}
+
+		public SubMenuModel CreateOpenFormMenuModel(string name, Func<Form> formFactory, Panel parent)
+		{
+			return new OpenFormSubMenuModel(formFactory, parent)
+			{
+				Name = name
+			};
+		}
+	}
+}
