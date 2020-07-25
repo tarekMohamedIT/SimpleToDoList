919b26f02b721af1349a44fe786c0f7cbf1f71f8
Tarek Mohamed <tarek.moahmmed@synceg.com>
2020-07-25 17:57:15 +0200

    Added new models for sectioned check lists

---
+++ ToDoList.Core/Models/CheckListSection.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.Core.Models
+{
+	public class CheckListSection
+	{
+		public string Title { get; set; }
+		public List<ChecklistItem> Items { get; set; }
+	}
+}
---
+++ ToDoList.Core/Models/SectionedCheckList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoList.Core.Models
+{
+	public class SectionedCheckList : BaseNote
+	{
+		public List<CheckListSection> Sections { get; set; }
+	}
+}
